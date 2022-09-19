9ffb7abd655e1d6f49444fbf12e63e19316d08f5
eldyyuda <eldyyudakurniawan@gmail.com>
2022-09-19 17:50:10 +0700

    first commit

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\Auth\RegisterRequest;
+
+class RegisterController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(RegisterRequest $request)
+    {
+        // request()->validate(
+        //     [
+        //         'name' => 'required|min:1|max:25',
+        //         'username' => 'required|alpha_num|min:3|max:255|unique:users,username',
+        //         'email' => 'required|email|unique:users,email',
+        //         'password' => 'required|min:6'
+        //     ]
+        // );
+        User::create([
+            'name' => request('name'),
+            'username' => request('username'),
+            'email' => request('email'),
+            'password' => request('password')
+        ]);
+        return response('Thank you for reqister');
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+// Route::prefix('api')->group(function () {
+Route::post('register', [RegisterController::class, '__invoke']);
+Route::post('login', [LoginController::class, '__invoke']);
+// });
